91e74e3a07038783638221b911f4fdde0241d898
hira480 <hiramannan8@gmail.com>
2022-05-12 00:38:55 +0600

    home page appointment added

--- src/Pages/Home/Exceptional.js
+++ src/Pages/Home/Exceptional.js
@@ -6,11 +6,13 @@ const Exceptional = () => {
     return (
         <div class="hero min-h-screen">
             <div class="hero-content grid grid-cols-1 lg:grid-cols-2 gap-4">
-                <img src={treatment} class="max-w-sm rounded-lg shadow-2xl" />
+                <div>
+                    <img src={treatment} class="max-w-sm rounded-lg shadow-2xl" />
+                </div>
                 <div>
                     <h1 class="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
                     <p class="py-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
-                    <PrimaryButton></PrimaryButton>
+                    <PrimaryButton>Get Started</PrimaryButton>
                 </div>
             </div>
         </div>
--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -9,7 +9,7 @@ const Info = () => {
     return (
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
             <InfoCard bgClass='bg-gradient-to-r from-secondary to-primary' cardTitel='Opening Hours' detail='Lorem Ipsum is simply dummy text of the pri' img={clock}></InfoCard>
-            <InfoCard bgClass="bg-accent" cardTitel='Our Location' detail='Brooklyn, NY 10036, United States' img={marker}></InfoCard>
+            <InfoCard bgClass="bg-neutral" cardTitel='Our Location' detail='Brooklyn, NY 10036, United States' img={marker}></InfoCard>
             <InfoCard bgClass='bg-gradient-to-r from-secondary to-primary' cardTitel='Contact Us' detail='+000 123 456789' img={phone}></InfoCard>
         </div>
     );
