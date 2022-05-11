e7cf5892e56f9c7fa9b4c46c2f592b02a2ef15be
hira480 <hiramannan8@gmail.com>
2022-05-11 23:28:51 +0600

    our services added

---
+++ src/Pages/Home/Exceptional.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import treatment from '../../assets/images/treatment.png';
+import PrimaryButton from '../Shared/PrimaryButton';
+
+const Exceptional = () => {
+    return (
+        <div class="hero min-h-screen">
+            <div class="hero-content grid grid-cols-1 lg:grid-cols-2 gap-4">
+                <img src={treatment} class="max-w-sm rounded-lg shadow-2xl" />
+                <div>
+                    <h1 class="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
+                    <p class="py-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                    <PrimaryButton></PrimaryButton>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Exceptional;
---
+++ src/Pages/Home/MakeAppoinment.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import doctor from '../../assets/images/doctor.png';
+
+const MakeAppoinment = () => {
+    return (
+        <section>
+            <div>
+                <img src={doctor} alt="" />
+            </div>
+            <div></div>
+        </section>
+    );
+};
+
+export default MakeAppoinment;
