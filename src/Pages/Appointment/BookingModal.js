a22b5f198a8558cf2256405f3f06242efb81bedd
hira480 <hiramannan8@gmail.com>
2022-05-15 11:41:52 +0600

    react query added for fetch data

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -4,7 +4,7 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { toast } from 'react-toastify';
 
-const BookingModal = ({ date, treatment, setTreatment }) => {
+const BookingModal = ({ date, treatment, setTreatment, refetch }) => {
     const { _id, name, slots } = treatment;
     const [user, loading, error] = useAuthState(auth);
     const formatedDate = format(date, 'PP');
@@ -39,9 +39,9 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
                 else {
                     toast.error(`Already have an Appointment on ${data.booking?.date} at ${data.booking?.slot}`);
                 }
+                refetch();
                 setTreatment(null);
             })
-
 
     }
 
